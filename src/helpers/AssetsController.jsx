19733ac8cc7974965676d95f5dfedc71a635f29d
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2024-01-18 12:55:28 -0500

    fix: images location

--- src/helpers/AssetsController.jsx
+++ src/helpers/AssetsController.jsx
@@ -2,42 +2,42 @@ export const Images = [
     {
         color: 'green',
         type: 'frog',
-        image: '../assets/frog-clipart-md.png'
+        image: '../../assets/frog-clipart-md.png'
     },
     {
         color: 'transparent',
         type: 'empty',
-        image: '../assets/empty.png'
+        image: '../../assets/empty.png'
     },
     {
         color: 'green',
         type: 'lilypad',
-        image: '../assets/lily-pad-clipart-md.png',
+        image: '../../assets/lily-pad-clipart-md.png',
     },
     {
         color: 'blue',
         type: 'flower',
-        image: '../assets/lily-clipart-md.png',
+        image: '../../assets/lily-clipart-md.png',
     },
     {
         color: 'pink',
         type: 'flower',
-        image: '../assets/lotus-clipart-2-md.png',
+        image: '../../assets/lotus-clipart-2-md.png',
     },
     /*
     {
         color: 'rose',
         type: 'flower',
-        image: '../assets/lotus-clipart-md.png',
+        image: '../../assets/lotus-clipart-md.png',
     },*/
     {
         color: 'white',
         type: 'flower',
-        image: '../assets/water-lily-clipart-md.png',
+        image: '../../assets/water-lily-clipart-md.png',
     },
     {
         color: 'blue',
         type: 'dragonfly',
-        image: '../assets/dragonfly-clipart-md.png',
+        image: '../../assets/dragonfly-clipart-md.png',
     }
 ]
