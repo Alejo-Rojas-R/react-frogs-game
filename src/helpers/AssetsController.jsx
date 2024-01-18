602f0da840ceb7cf2821f8cb53c9b8c99bf1f7da
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2024-01-18 12:48:58 -0500

    refactor: images location

--- src/helpers/AssetsController.jsx
+++ src/helpers/AssetsController.jsx
@@ -2,42 +2,42 @@ export const Images = [
     {
         color: 'green',
         type: 'frog',
-        image: 'src/assets/frog-clipart-md.png'
+        image: '../assets/frog-clipart-md.png'
     },
     {
         color: 'transparent',
         type: 'empty',
-        image: 'src/assets/empty.png'
+        image: '../assets/empty.png'
     },
     {
         color: 'green',
         type: 'lilypad',
-        image: 'src/assets/lily-pad-clipart-md.png',
+        image: '../assets/lily-pad-clipart-md.png',
     },
     {
         color: 'blue',
         type: 'flower',
-        image: 'src/assets/lily-clipart-md.png',
+        image: '../assets/lily-clipart-md.png',
     },
     {
         color: 'pink',
         type: 'flower',
-        image: 'src/assets/lotus-clipart-2-md.png',
+        image: '../assets/lotus-clipart-2-md.png',
     },
     /*
     {
         color: 'rose',
         type: 'flower',
-        image: 'src/assets/lotus-clipart-md.png',
+        image: '../assets/lotus-clipart-md.png',
     },*/
     {
         color: 'white',
         type: 'flower',
-        image: 'src/assets/water-lily-clipart-md.png',
+        image: '../assets/water-lily-clipart-md.png',
     },
     {
         color: 'blue',
         type: 'dragonfly',
-        image: 'src/assets/dragonfly-clipart-md.png',
+        image: '../assets/dragonfly-clipart-md.png',
     }
 ]
